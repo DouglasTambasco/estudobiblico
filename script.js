73a8220f27858289358e05e46f7d9db3ee8a9870
Douglas Tambasco <135037210+DouglasTambasco@users.noreply.github.com>
2026-04-28 10:59:19 -0300

    v1.3.1 (testes)

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
 // Firebase Setup
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import {
-  getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile,
-  onAuthStateChanged, signOut, sendEmailVerification, GoogleAuthProvider, signInWithPopup,
-  sendPasswordResetEmail
-} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import {
-  getFirestore, collection, doc, setDoc, updateDoc, deleteDoc, getDocs, query, where, serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { auth, db } from "./firebase.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDSy_V62ZUXK-2E1H05uTbvLvM9Q6D_Lng",
-  authDomain: "estudobiblico-1b794.firebaseapp.com",
-  projectId: "estudobiblico-1b794",
-  storageBucket: "estudobiblico-1b794.appspot.com",
-  messagingSenderId: "92626454313",
-  appId: "1:92626454313:web:ac8cbded596cb179265938",
-  measurementId: "G-P927QCEMQM"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 let marcacoesSelecionadas = [];
 
 // Elementos fixos
